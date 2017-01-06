e602d80450e0d0568e89eb23f6cbdc849938f5f8
Drew Hornbein <dhornbein@gmail.com>
2017-01-06 00:18:49 -0500

    home page hero setup

---
+++ loop-templates/home-hero.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Partial template for home page hero post
+ *
+ * @package understrap
+ */
+
+// featured image
+// TODO put in a default image if none exists
+$thumb = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'full' );
+?>
+<article <?php post_class('post-hero row'); ?> id="post-<?php the_ID(); ?>" style="background-image: url('<?php echo $thumb['0'];?>')">
+
+	<div class="col-md-12">
+		<header class="entry-header">
+		
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		
+		</header><!-- .entry-header -->
+		
+		<div class="entry-content">
+		
+			<?php the_content(); ?>
+		
+			<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+				'after'  => '</div>',
+			) );
+			?>
+		
+		</div><!-- .entry-content -->
+		
+		<footer class="entry-footer">
+		
+			<?php edit_post_link( __( 'Edit', 'understrap' ), '<span class="edit-link">', '</span>' ); ?>
+		
+		</footer><!-- .entry-footer -->
+	</div>
+
+</article><!-- #post-## -->
