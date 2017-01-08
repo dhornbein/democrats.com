3fdf2e6b66eab69e43f2df4921025e8163f3eb3e
Drew Hornbein <dhornbein@gmail.com>
2017-01-08 14:07:19 -0500

    home post gutters

--- loop-templates/home-post.php
+++ loop-templates/home-post.php
@@ -6,31 +6,33 @@
  */
 
 ?>
-<article <?php post_class('col-md-4'); ?> id="post-<?php the_ID(); ?>">
-
-	<header class="entry-header">
-
-		<?php the_title( '<h3 class="entry-title">', '</h3>' ); ?>
-
-	</header><!-- .entry-header -->
-
-	<div class="entry-content">
-
-		<?php the_content(); ?>
-
-		<?php
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-			'after'  => '</div>',
-		) );
-		?>
-
-	</div><!-- .entry-content -->
-
-	<footer class="entry-footer">
-
-		<?php edit_post_link( __( 'Edit', 'understrap' ), '<span class="edit-link">', '</span>' ); ?>
-
-	</footer><!-- .entry-footer -->
-
-</article><!-- #post-## -->
+<div class="col-md-4">
+	<article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
+	
+		<header class="entry-header">
+	
+			<?php the_title( '<h5 class="entry-title">', '</h5>' ); ?>
+	
+		</header><!-- .entry-header -->
+	
+		<div class="entry-content">
+	
+			<?php the_content(); ?>
+	
+			<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+				'after'  => '</div>',
+			) );
+			?>
+	
+		</div><!-- .entry-content -->
+	
+		<footer class="entry-footer">
+	
+			<?php edit_post_link( __( 'Edit', 'understrap' ), '<span class="edit-link">', '</span>' ); ?>
+	
+		</footer><!-- .entry-footer -->
+	
+	</article><!-- #post-## -->
+</div>
