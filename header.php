cd18700835eec918158bfa0180b92ec21a0d9c80
Drew Hornbein <dhornbein@gmail.com>
2017-01-08 14:07:40 -0500

    added bold font weight from google

--- header.php
+++ header.php
@@ -21,7 +21,7 @@ $container = get_theme_mod( 'understrap_container_type' );
 	      content="<?php bloginfo( 'name' ); ?> - <?php bloginfo( 'description' ); ?>">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
-	<link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Montserrat:400,700" rel="stylesheet">
 	<?php wp_head(); ?>
 </head>
 
