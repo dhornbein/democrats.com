258ba95d1ef849814bf312ff5f05ba1ebf4d23ed
Drew Hornbein <dhornbein@gmail.com>
2017-01-08 20:19:48 -0500

    single layout

--- loop-templates/content-single.php
+++ loop-templates/content-single.php
@@ -8,36 +8,48 @@
 ?>
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
 
-	<header class="entry-header">
+	<div class="row">
+		<div class="col-xs-12">
+			<header class="entry-header text-xs-center">
+			
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			
+				<div class="entry-meta">
+			
+					<?php echo understrap_social(); ?>
+			
+					<?php understrap_posted_on(); ?>
+			
+				</div><!-- .entry-meta -->
+			
+			</header><!-- .entry-header -->
+		</div>
+	</div>
 
-		<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	<div class="entry-content row">
 
-		<div class="entry-meta">
+		<div class="col-md-8 offset-md-2">
 
-			<?php understrap_posted_on(); ?>
+			<?php the_content(); ?>
+			
+			<?php
+			wp_link_pages( array(
+				'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
+				'after'  => '</div>',
+			) );
+			?>
 
-		</div><!-- .entry-meta -->
-
-	</header><!-- .entry-header -->
-
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
-
-	<div class="entry-content">
-
-		<?php the_content(); ?>
-
-		<?php
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-			'after'  => '</div>',
-		) );
-		?>
+		</div>
 
 	</div><!-- .entry-content -->
 
-	<footer class="entry-footer">
+	<footer class="entry-footer row">
 
-		<?php understrap_entry_footer(); ?>
+		<div class="col-md-8 offset-md-2 text-xs-center">
+			<?php echo understrap_social('3x'); ?>
+
+			<?php understrap_entry_footer(); ?>
+		</div>
 
 	</footer><!-- .entry-footer -->
 
